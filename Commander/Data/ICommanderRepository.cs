3048443bd47216d2948e860e24fbfdb10d21cd5a
Mahmoud Rhazzoul <56742148+butbanksy@users.noreply.github.com>
2020-12-17 20:25:05 +0100

    Created the ICommanderRepository and a Mock implementation of it

---
+++ Commander/Data/ICommanderRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Commander.Models;
+
+namespace Commander.Data
+{
+    public interface ICommanderRepository
+    {
+        IEnumerable<Command> GetAllCommands();
+        Command GetCommandById(int id);    
+        
+    }
+}
---
+++ Commander/Data/MockCommanderRepository.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Commander.Models;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepository : ICommanderRepository
+    {
+        public IEnumerable<Command> GetAllCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command()
+                {
+                    HowTo = "Boil an egg",
+                    Id = 1,
+                    Line = "boil egg",
+                    Platform = "Mac OS X"
+                },
+                new Command()
+                {
+                    HowTo = "Boil an egg",
+                    Id = 1,
+                    Line = "boil egg",
+                    Platform = "Mac OS X"
+                },
+            };
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command()
+            {
+                HowTo = "Boil an egg",
+                Id = 1,
+                Line = "boil egg",
+                Platform = "Mac OS X"
+            };
+        }
+    }
+}
