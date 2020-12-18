a40bc1d12b3a7600b4d42c9fddf1311ede1e483f
Mahmoud Rhazzoul <56742148+butbanksy@users.noreply.github.com>
2020-12-18 03:05:02 +0100

    Created CommanderContext and migrations. Added new SqlCommanderRepository implementation

---
+++ Commander/Data/SqlCommanderRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Commander.Models;
+
+namespace Commander.Data
+{
+    public class SqlCommanderRepository : ICommanderRepository
+    {
+        private readonly CommanderContext _commanderContext;
+
+        public SqlCommanderRepository(CommanderContext commanderContext)
+        {
+            _commanderContext = commanderContext;
+        }
+        public IEnumerable<Command> GetAllCommands()
+        {
+            return _commanderContext.Commands.ToList();
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return _commanderContext.Commands.FirstOrDefault(command => command.Id == id);
+        }
+    }
+}
